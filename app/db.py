27a7e7c9fb0f0b501c2a9da9fa75d769be4709c7
Patrick <patcerutti23@gmail.com>
2023-02-05 14:34:49 -0700

    database and app running with docker

---
+++ app/db.py
@@ -0,0 +1,26 @@
+import os
+from psycopg_pool import ConnectionPool
+
+
+pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
+
+
+class UserQueries:
+    def get_all_users(self):
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    SELECT id, first, last, avatar,
+                        email, username
+                    FROM users
+                    ORDER BY last, first
+                """)
+
+                results = []
+                for row in cur.fetchall():
+                    record = {}
+                    for i, column in enumerate(cur.description):
+                        record[column.name] = row[i]
+                    results.append(record)
+
+                return results
